61ac0d97a7de1ce7f4f3c12377bc755feb00b005
Sagar Munagekar <code@dailyascent.net>
2025-06-24 19:56:45 +0530

    update

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,7 @@
-terraform {
+/* terraform {
+  cloud {
+    hostname
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -6,5 +9,46 @@ terraform {
     }
   }
   required_version = ">= 0.14.0"
+} */
+
+terraform {
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "fm-dev"
+    workspaces {
+      name = "learn-terraform-dev"
+
+    }
+  }
 }
 
+
+
+/* terraform {
+  required_version = "<version>"
+  required_providers {
+    <PROVIDER> {
+      version = "<version-constraint>"
+      source = "<provider-address>"
+    }
+  }
+  provider_meta "<LABEL>" { 
+    # Shown for completeness but only used for specific cases     
+  }
+  backend "<TYPE>" {        
+    # `backend` is mutually exclusive with `cloud` 
+    "<ARGUMENTS>"
+  }
+  cloud {                   
+    # `cloud` is mutually exclusive with `backend` 
+    organization = "<organization-name>"
+    workspaces {
+      tags = [ "<tag>" ]
+      name = "<workspace-name>"
+      project = "<project-name>"
+    }
+    hostname = "app.terraform.io"
+    token - "<TOKEN>"
+  }
+  experiments = [ "<feature-name>" ]
+} */
